cd8ce732f1173c455c32fea4d4fe8af4e2f64d57
Nico <nico.aldag@web.de>
2023-07-28 09:16:55 +0200

    Lösung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,31 @@ public class App {
     
     public static void main(String[] args) {
     
+    
+    Konto konto1 = new Konto(10000);
+    Konto konto2 = new Konto(10000);
+    Konto konto3 = new Konto(10000);
+    
+    output(konto1.getKontostand());
+    output(konto2.getKontostand());
+    output(konto3.getKontostand());
+   
+    output("-------------------");
+
+    konto1.setKontostand(konto1.getKontostand() * 2);
+    konto2.setKontostand(konto2.getKontostand() * 3);
+    konto3.setKontostand(konto3.getKontostand() * 10);
+
+    output(konto1.getKontostand());
+    output(konto2.getKontostand());
+    output(konto3.getKontostand());
+
     }
-
-
     private static void output(String outputStr) {
         System.out.println(outputStr);
     } 
 
+    private static void output(int outputInt) {
+        System.out.println(outputInt);
+    } 
 }
